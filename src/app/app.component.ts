66b0f17050e980ebf398c2e81492b3a12a32958d
julianourias <dev.urias@gmail.com>
2024-07-25 23:35:57 -0300

    style modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = 'Juliano Urias';
+  name = 'Juliano Augusto Cavalcante Urias';
 }
