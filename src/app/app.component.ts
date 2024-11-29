3ed97ffacec0f47dd30bd7daaa34b2bb6e5fa53f
julianourias <dev.urias@gmail.com>
2024-11-28 21:53:49 -0300

    feat: update informations and meta data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,39 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name = 'Juliano Augusto Cavalcante Urias';
 
- /*
-  <div class="job">
-        <h4>Analista de Desenvolvimento - Equipe de IA</h4>
-        <p><strong>Mutant</strong>, Brasil (Ago. 2024 - Jul. 2024)</p>
-        <p>Atualmente, estou iniciando em uma nova oportunidade para atuar no time de Inteligência Artificial da cliente Vivo (Telefónica).</p>
-        <p><strong>Habilidades Técnicas:</strong> Python, IA, React, Kubernetes, GitLab, MongoDB, LangChain</p>
-      </div>
-      <div class="job">
-        <h4>Engenheiro de Software - Equipe de Inovação</h4>
-        <p><strong>UOL</strong>, Brasil (Jun. 2024 - Nov. 2023)</p>
-        <p>Fiz parte da equipe de inovação do UOL, responsável pelo desenvolvimento de novos produtos dentro da empresa.</p>
-        <p><strong>Habilidades Técnicas:</strong> Python, IA, React (com Next), AWS, Kubernetes, Jenkins, SQL (MySQL), Firebase, Linux</p>
-      </div>
-      <div class="job">
-        <h4>Engenheiro de Software - Equipe UOL Ads</h4>
-        <p><strong>UOL</strong>, Brasil (Out. 2023 - Mai. 2022)</p>
-        <p>Iniciei minha jornada no UOL na equipe UOL Ads, a plataforma de publicidade self-service do UOL...</p>
-        <p><strong>Habilidades Técnicas:</strong> Java (com Spring), Vue.js, Node.js, Angular, Docker, Kubernetes, Jenkins, SQL (MySQL), RabbitMQ, Linux</p>
-      </div>
-      <div class="job">
-        <h4>Programador Full Stack - Analista de Sistemas</h4>
-        <p><strong>KMM by nstech</strong>, Brasil (Mai. 2022 - Abr. 2021)</p>
-        <p>Trabalhei desenvolvendo soluções/customizações para algumas das maiores empresas de logística do Brasil.</p>
-        <p><strong>Habilidades Técnicas:</strong> Angular, TypeScript, CSS, Testes unitários com utPLSQL, Python, Java, Docker, Apache Airflow, GIT, GitHub, Subversion</p>
-      </div>
-      <div class="job">
-        <h4>Programador Full Stack - Analista de Sistemas</h4>
-        <p><strong>Dominus Software</strong>, Paraná, Brasil (Abr. 2021 - Jan. 2020)</p>
-        <p>Nessa função, trabalhei principalmente no desenvolvimento e manutenção de aplicativos mobile usando o framework React Native.</p>
-        <p><strong>Habilidades Técnicas:</strong> React Native, Node.js, Delphi, GIT, GitHub</p>
-      </div>
- */
-
   jobs: Job[] = [
     {
       title: 'Desenvolvedor de Aplicações - IBM Consulting',
@@ -49,8 +16,8 @@ export class AppComponent {
       location: 'Brasil',
       start_date: 'Ago. 2024',
       end_date: 'Presente',
-      description: 'Atualmente, estou iniciando em uma nova oportunidade na área de consultoria da IBM.',
-      skills: 'Python, Angular'
+      description: 'Atuando na área de consultoria da IBM, alocado em um projeto da Petrobrás, no qual atuo como desenvolvedor full-stack utilizando a stack Python e Angular.',
+      skills: 'Python, Angular, PL/SQL, AWS'
     },
     {
       title: 'Analista de Desenvolvimento - Equipe de IA',
